bd1c22fb9a115a62a794a9a3ba1ec5a63426d714
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-02-13 03:07:54 +0200

    Refactors reading list routes for improved organization, adds functionality for managing notes and duplicating reading lists, and standardizes key naming conventions in PostResource.

--- routes/api.php
+++ routes/api.php
@@ -244,17 +244,21 @@ Route::prefix('v1')->middleware('throttle:15,1')->group(function () {
         // Reading Lists
         Route::controller(ReadingListController::class)->group(function () {
             Route::get('reading-lists/lists/posts', 'index');
-            //            Route::get('reading-lists/', 'Lists');
+
             Route::post('reading-lists', 'store');
             Route::get('reading-lists/{readingList}', 'show');
             Route::patch('reading-lists/{readingList}', 'update');
             Route::delete('reading-lists/{readingList}', 'destroy');
+            // Manage posts in reading list
             Route::post('reading-lists/{readingList}/add-post/{post}', 'addPostToReadingList');
             Route::delete('reading-lists/{readingList}/remove-post/{post}', 'removePostFromReadingList');
             Route::post('reading-lists/{readingList}/move-post/{post}', 'movePostToAnotherList');
+            // Manage notes for posts in reading list
             Route::post('reading-lists/{readingList}/add-note/{post}', 'addNoteToPostInReadingList');
             Route::delete('reading-lists/{readingList}/delete-note/{post}', 'deleteNoteInPostInReadingList');
+            // Duplicate reading list
             Route::post('reading-lists/{readingList}/duplicate', 'duplicateReadingList');
+
             Route::get('reading-lists/{readingList}/show-notes/{post}', 'showNotesInReadingList');
         });
 
