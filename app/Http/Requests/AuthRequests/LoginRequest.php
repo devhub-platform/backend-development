4f8f730d4a8e794f8b3ee463cfa2cde6f69f3793
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-02-20 09:00:44 +0200

    Update API versioning and enhance email validation rules in login request

--- app/Http/Requests/AuthRequests/LoginRequest.php
+++ app/Http/Requests/AuthRequests/LoginRequest.php
@@ -10,7 +10,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email', 'max:254'],
+            'email' => ['required', 'email:rfc,dns', 'max:254'],
             'password' => ['required', Password::defaults(), 'string'],
             'remember_me' => ['nullable', 'boolean'],
         ];
