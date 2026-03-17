38e4dc0d053f64b537c886c7b52218ba7b4a18f6
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-03-17 20:58:42 +0200

    Refactor API routes and update web route for homepage
    - Added TestNotificationController to API routes for handling message notifications.
    - Simplified the route for sending message notifications by removing redundant entries.
    - Changed the homepage route to return a welcome view instead of redirecting to an external URL.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\Auth\SocialiteMediaFrontController;
 
-Route::redirect('/', 'https://devhub-platform.github.io/frontend-development/');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/chats', function () {
     return view('Chat-users.test-pusher-chat');
