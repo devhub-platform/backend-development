bac3dc605c429afde5bf269ee8a990cbee1160c6
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-04-13 22:48:28 +0200

    Integrate OneSignal push notifications across user interactions (auth, posts, comments, followers, and reactions); update API routes creator metadata

--- app/Http/Controllers/V1/ReactionController.php
+++ app/Http/Controllers/V1/ReactionController.php
@@ -8,6 +8,7 @@ use App\Notifications\ReactNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Notification;
+use OneSignal;
 
 class ReactionController
 {
@@ -37,6 +38,16 @@ class ReactionController
             $user->updateReaction($validated['type'], $post);
             if ($post->user->isNotificationEnabled('new_reaction')) {
                 Notification::send($post->user, new ReactNotification($post, $validated['type'], $user));
+                OneSignal::sendNotificationToUser(
+                    'Your post received a new reaction',
+                    $post->user->onesignal_player_id,
+                    'deeplink://reactions?id=' . $post->id,
+                    null,
+                    null,
+                    null
+                    ,
+                    'Your post received a new reaction'
+                );
             }
 
             return response()->json([
