48be41e681041a9a034f18c8d8672f6c1f42e5e7
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-04-17 21:54:19 +0200

    Update middleware to refine trusted proxy headers configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
+        $middleware->trustProxies(
+        at: '*',
+        headers: Request::HEADER_X_FORWARDED_FOR
+            | Request::HEADER_X_FORWARDED_HOST
+            | Request::HEADER_X_FORWARDED_PORT
+            | Request::HEADER_X_FORWARDED_PROTO
+    );
 
         $middleware->alias([
             'blocked.user' => BlockedUserMiddleware::class,
