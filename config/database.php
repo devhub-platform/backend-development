44a6700c7f848cbf6209437265a29811dc97b275
Youssef Ahmed <youssef419386@fci.bu.edu.eg>
2026-05-03 15:55:27 +0300

    Refactor database.php: Uncomment 'laravel-visits' configuration and add SSL options for Redis connection

--- config/database.php
+++ config/database.php
@@ -147,7 +147,7 @@ return [
     | such as Memcached. You may define your connection settings here.
     |
     */
-//    'laravel-visits' => [
+    //    'laravel-visits' => [
 //        'host' => env('REDIS_HOST', '127.0.0.1'),
 //        'password' => env('REDIS_PASSWORD', null),
 //        'port' => env('REDIS_PORT', 6379),
@@ -160,7 +160,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string)env('APP_NAME', 'laravel')) . '-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
@@ -176,7 +176,13 @@ return [
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
             'scheme' => env('REDIS_SCHEME', 'tls'),
-            
+
+            'options' => [
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
         ],
 
         'cache' => [
@@ -190,7 +196,7 @@ return [
             'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
-//            'schema' => env('SC')
+            //            'schema' => env('SC')
         ],
 
     ],
